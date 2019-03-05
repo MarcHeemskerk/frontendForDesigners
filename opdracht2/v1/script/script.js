a77882fb6f126bbfbe1c8934ddea41e9ac9e06f2
X-Track <xxttrack@gmail.com>
2019-03-05 08:40:53 +0100

    v2

--- opdracht2/v1/script/script.js
+++ opdracht2/v1/script/script.js
@@ -1,67 +1,156 @@
-var getImg0 = document.getElementById("cover0");
-var getImg1 = document.getElementById("cover1");
-var getImg2 = document.getElementById("cover2");
-var getImg3 = document.getElementById("cover3");
-var getImg4 = document.getElementById("cover4");
-var getImg5 = document.getElementById("cover5");
-var getImg6 = document.getElementById("cover6");
-var getImg7 = document.getElementById("cover7");
+// triangulation using https://github.com/ironwallaby/delaunay
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
 
 
-function liked0() {
+var btn = document.querySelectorAll("button");
+var images = document.querySelectorAll(".photos");
+var ShowSlide = 1;
 
-    getImg0.src = "../animation/interaction.gif";
 
+function showImages(nummer) {
+
+    /* ____ Change Value of ShowSlide ____ */
+    if (nummer > images.length) {
+        ShowSlide = 1;
+    } else if (nummer < 1) {
+        ShowSlide = images.length;
+    }
+
+
+
+    /* ____ Set all images Display None ____ */
+    var i;
+    for (i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+
+    /* ____ Set 1 image on display Block ____ */
+    images[ShowSlide - 1].style.display = "block";
+
+
+
+    /*------ CONSOLE.LOG ------*/
+    console.log(images[ShowSlide - 1].alt);
 }
 
-function liked1() {
 
-    getImg1.src = "../animation/interaction.gif";
-
+function buttonPress(nummer) {
+    /***** ACTION *****/
+    showImages(ShowSlide += nummer);
 }
 
-function liked2() {
 
-    getImg2.src = "../animation/interaction.gif";
 
+
+//https://stackoverflow.com/questions/2373995/javascript-addeventlistener-event-fires-on-page-load
+
+/*
+el.addEventListener("click", alert("clicktrack"), false);
+When this line is executed, the alert will be called and return undefined. To pass the alert code you need to wrap it in a function.
+*/
+
+function min1() {
+    /*------ CONSOLE.LOG ------*/
+    console.log("-1");
+
+    /***** ACTION *****/
+    buttonPress(-1);
 }
 
-function liked3() {
+function plus1() {
+    /*------ CONSOLE.LOG ------*/
+    console.log("+1");
 
-    getImg3.src = "../animation/interaction.gif";
-
+    /***** ACTION *****/
+    buttonPress(+1);
 }
 
-function liked4() {
+btn[0].addEventListener("click", min1);
+btn[1].addEventListener("click", plus1);
 
-    getImg4.src = "../animation/interaction.gif";
 
+
+//https://stackoverflow.com/questions/5597060/detecting-arrow-key-presses-in-javascript
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+      min1();
+    }
+    else if (e.keyCode == '39') {
+       plus1();
+    }
 }
 
-function liked5() {
 
-    getImg5.src = "../animation/interaction.gif";
 
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+document.addEventListener("keydown", function(event) {
+
+  if (event == "37") {
+        min1();
+    } else if (event == "39") {
+        plus1();
+    }
+    console.log(event.which);
+});
+
+*/
+
+
+/*
+
+var currentImg = 1;
+var allImgs = document.querySelectorAll(".mySlides");
+var btnLeft = document.getElementById("buttonLeft");
+var btnRight = document.getElementById("buttonRight");
+
+function showImg(nummer) {
+
+console.log(nummer);
+    if (nummer > allImgs.length) {
+        currentImg = 1;
+    }
+
+    else if (nummer < 1) {
+        currentImg = allImgs.length;
+    }
+
+    var i;
+    for (i = 0; i < allImgs.length; i++) {
+        allImgs[i].style.display = "none";
+    }
+
+    allImgs[currentImg -1].style.display = "block";
 }
 
-function liked6() {
-
-    getImg6.src = "../animation/interaction.gif";
-
+function plusOfMin(nummer) {
+    console.log(nummer);
+    showImg(currentImg += nummer);
 }
 
-function liked7() {
+showImg(currentImg);
 
-    getImg7.src = "../animation/interaction.gif";
+btnLeft.addEventListener("click", plusOfMin,(-1));
+btnRight.addEventListener("click", plusOfMin,(1));
 
-}
+console.log(allImgs);
 
-getImg0.addEventListener("click", liked0);
-getImg1.addEventListener("click", liked1);
-getImg2.addEventListener("click", liked2);
-getImg3.addEventListener("click", liked3);
-getImg4.addEventListener("click", liked4);
-getImg5.addEventListener("click", liked5);
-getImg6.addEventListener("click", liked6);
-getImg7.addEventListener("click", liked7);
-
+*/
